5e8fad763c4c3f92185dab689236c2913e0e5c4b
eddard-contessa <elizabeth.tsenker@gmail.com>
2025-02-03 20:47:56 +0100

    USERHOST checked witm irc man, solved problems in UserCommand

--- srcs/cmds/UserCommand.cpp
+++ srcs/cmds/UserCommand.cpp
@@ -4,93 +4,48 @@
 UserCommand::UserCommand(Server* server) : Command(server) {}
 
 void	UserCommand::execute(Client* client, const std::vector<std::string>& args) {
-	if (client->getState() == UNAUTHENTICATED) {
-		client->reply(":" + _server->getHostname()
-					+ " 451 " + client->getNickname() + " USER :You have not registered");
-		// client->reply(":server 451 USER :You have not registered");
-		return ;
-	}
+    if (client->isUsernameSet()) {
+        client->reply(":" + _server->getHostname()
+                    + " 462 " + client->getNickname() + " USER :You may not reregister");
+        return ;
+    }
 
-	if (!client->isNicknameSet()) {
-		client->reply(":" + _server->getHostname()
-					+ " 461 " + client->getNickname() + " USER :You must set a nickname first");
-		// client->reply(":server 461 USER :You must set a nickname first");
-		return ;
-	}
+    if (!client->isNicknameSet()) {
+        client->reply(":" + _server->getHostname()
+                    + " 461 " + client->getNickname() + " USER :You must set a nickname first");
+        return ;
+    }
 
-	if (client->isUsernameSet()) {
-		client->reply(":" + _server->getHostname()
-					+ " 462 " + client->getNickname() + " USER :You may not reregister");
-		client->reply(":server 462 USER :You may not reregister");
-		return ;
-	}
+    if (args.size() < 4) {
+        client->reply(":" + _server->getHostname() 
+                    + " 461 " + client->getNickname() + " USER :Not enough parameters");
+        return ;
+    }
 
-	// if (args.empty()) { // пока делаю тесты с одной командой, но вообще вроде как должно быть четыре. пока не разобралась на сколько это важно 4 аргумента
-	// 	// client->reply(":server 461 USER :Not enough parameters");
-	// 	client->reply(":" + _server->getHostname() 
-	// 				+ " 461 " + client->getNickname() + " USER :Not enough parameters");
-	// 	return ;
-	// }
+    if (args[0].size() > 9) {
+        client->reply(":" + _server->getHostname() + " 432 " + client->getNickname()
+                    + " USER :Username is too long (max 9 characters)");
+        return ;
+    }
 
-	if (args.size() < 4) {
-		client->reply(":" + _server->getHostname() 
-					+ " 461 " + client->getNickname() + " USER :Not enough parameters");
-		// client->reply(":server 461 USER :Not enough parameters");
-		return ;
-	}
+    client->setUsername(args[0]);
+    client->setUsernameSet(true);
 
-	if (args[0].size() > 9) {
-		client->reply(":" + _server->getHostname() + " 432 " + client->getNickname()
-					+ " USER :Username is too long (max 9 characters)");
-		return ;
-	}
+    std::string realname;
+    if (args[3][0] == ':') { 
+        realname = args[3].substr(1);
+        for (size_t i = 4; i < args.size(); ++i) {
+            realname += " " + args[i];
+        }
+    } else {
+        realname = args[3];
+    }
 
-	client->setUsername(args[0]);
-	client->setUsernameSet(true);
-	std::string	realname;
-	if (args[3][0] == ':') { 
-		realname = args[3].substr(1);
-		for (size_t i = 4; i < args.size(); ++i) {
-			realname += " " + args[i];
-		}
-	} 
-	else
-		realname = args[3];
-	client->setRealName(realname);
+    client->setRealName(realname);
+    client->reply(":" + _server->getHostname() + " NOTICE " + client->getNickname() +
+                " :Username set successfully");
 
-	client->reply(":" + _server->getHostname() + " NOTICE " + client->getNickname() +
-				" :Username set successfully");
-	// client->reply(":server NOTICE " + client->getUsername() + " :Username set successfully");
-	// if (client->isNicknameSet() && client->isUsernameSet()) {
-	// 	client->registerAction(_server);
-	// }
-
-	// Если PASS, NICK и USER уже введены, клиент считается зарегистрированным
-	if (client->isNicknameSet() && client->isUsernameSet()) {
-		client->registerAction(_server);
-		// client->setState(REGISTERED);
-
-		// // Отправляем приветствие и код 001
-		// client->reply(":" + _server->getHostname() + " 001 " + client->getNickname() +
-		// 			" :Welcome to the IRC Network " + client->getNickname() + "!" +
-		// 			client->getUsername() + "@" + _server->getHostname());
-
-		// client->reply(":" + _server->getHostname() + " 002 " + client->getNickname() +
-		// 			" :Your host is " + _server->getHostname() + ", running version 1.0");
-
-		// std::stringstream	ss;
-		// ss << _server->getCreationTime();
-		// client->reply(":" + _server->getHostname() + " 003 " + client->getNickname() +
-		// 			" :This server was created " + ss.str());
-
-		// client->reply(":" + _server->getHostname() + " 004 " + client->getNickname() +
-		// 			" " + _server->getHostname() + " 1.0 o i k l b t");
-
-		// // Отправляем список каналов после подключения
-		// Command *listCmd = new ListCommand(_server);
-		// listCmd->execute(client, std::vector<std::string>());
-		// delete listCmd;
-	}
-
-	// _server->handleCommand(client, "LIST");
+    if (client->isNicknameSet() && client->isUsernameSet()) {
+        client->registerAction(_server);
+    }
 }
--- srcs/cmds/UserhostCommand.cpp
+++ srcs/cmds/UserhostCommand.cpp
@@ -2,21 +2,30 @@
 
 UserhostCommand::UserhostCommand(Server* server) : Command(server) {}
 
-void	UserhostCommand::execute(Client* client, const std::vector<std::string>& args) {
-	if (args.empty()) {
-		client->reply(":server 461 " + client->getNickname() + " USERHOST :Not enough parameters");
-		return ;
-	}
+void UserhostCommand::execute(Client* client, const std::vector<std::string>& args) {
+    if (args.empty()) {
+        client->reply(":server 461 " + client->getNickname() + " USERHOST :Not enough parameters");
+        return;
+    }
 
-	std::string info = "";
-	for(size_t i = 0; i < args.size(); i++) {
-		Client* c = _server->findClientByNickname(args[i]);
-		if (c == NULL) {
-			continue ;
-		}
-		if (i > 0)
-			info += " ";
-		info += (c->getUsername() + "=" + c->getUserSymbol() + c->getUsername() + "@" + c->getIpAddr());
-	}
-	client->reply(":server 302 " + client->getNickname() + " :" + info);
+    std::string info = "";
+    size_t count = args.size() > 5 ? 5 : args.size(); // IRC ограничивает USERHOST до 5 пользователей
+
+    for (size_t i = 0; i < count; i++) {
+        Client* c = _server->findClientByNickname(args[i]);
+        if (c == NULL) {
+            continue;
+        }
+
+        if (!info.empty()) {
+            info += " ";
+        }
+
+        std::string username = c->getUsername().empty() ? "unknown" : c->getUsername();
+        std::string host = c->getIpAddr().empty() ? "unknown" : c->getIpAddr(); 
+
+        info += c->getNickname() + "=" + username + "@" + host;
+    }
+
+    client->reply(":server 302 " + client->getNickname() + " :" + info);
 }
