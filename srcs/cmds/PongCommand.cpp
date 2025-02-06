554d6968f46b82f1caf6a489cd4268a3aabcddb4
N0fish <allagultseva@gmail.com>
2025-02-06 02:16:47 +0100

    PONG

--- srcs/cmds/PongCommand.cpp
+++ srcs/cmds/PongCommand.cpp
@@ -1,12 +1,21 @@
 #include "PongCommand.hpp"
+/*
+PONG command.
+Replies to PING to confirm activity. If a second parameter is provided,
+PONG is forwarded to that server.
 
+Команда PONG.
+Отвечает на PING, подтверждая активность клиента или сервера.
+Если указан второй параметр, PONG пересылается указанному серверу.
+*/
 PongCommand::PongCommand(Server* server) : Command(server) {}
 
 void	PongCommand::execute(Client* client, const std::vector<std::string>& args) {
+	std::string	host = _server->getHostname();
+	std::string	nick = client->getNickname();
 	if (args.empty()) {
-		client->reply(":server 409 PONG :No origin specified");
+		client->reply(ERR_NOORIGIN(host, nick));
 		return ;
 	}
-
 	client->updateActivity();
 }
